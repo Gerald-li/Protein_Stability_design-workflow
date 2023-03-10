97a9f6936a424692b0bd7d3c19839e22678b9fe6
Gerald-li <65849841+Gerald-li@users.noreply.github.com>
2023-03-10 18:43:50 +0800

    Update Step3_prep_CNA_files.d

--- Scripts/Step3_prep_CNA_files.d
+++ Scripts/Step3_prep_CNA_files.d
@@ -1,2 +1,4 @@
+CNA files can be obtained from the following website by submitting an InputPDB file, https://cpclab.uni-duesseldorf.de/cna/main.php
+
 Creat a directory named 'cna_results', and copy the '*local_indices.dat' file to this directory.
 		#for example "mv 1PS6_chainA_local_indices.dat cna_results"
