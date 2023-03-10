475e5fb57f51515a5c8a22973ff52f2916317c64
Gerald-li <65849841+Gerald-li@users.noreply.github.com>
2023-03-10 18:07:51 +0800

    Add files via upload

---
+++ Scripts/1PS6_chainA/G206A/mute.sh
@@ -0,0 +1,1 @@
+mutate.default.linuxgccrelease -s ../refine/1PS6_chainA_0001_0045.pdb -mutate:mutation G206A
---
+++ Scripts/1PS6_chainA/G206A/relax.sh
@@ -0,0 +1,1 @@
+mpirun --hostfile /usr/local/bin/hostfile -np 39 --allow-run-as-root relax.mpi.linuxgccrelease -s 1PS6_chainA_0001_0045_G206A.pdb @refineflag
